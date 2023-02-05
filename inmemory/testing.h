132c7be920652285d4d7268a33d650a96dddfaf9
Rob Tierolff <rob.tierolff@centric.eu>
2023-02-05 23:07:23 +0100

    folder structure

--- inmemory/testing.h
+++
@@ -1,5 +0,0 @@
-void char_equals(char *gherkin, char actual, char expected);
-void int_equals(char *gherkin, int actual, int expected);
-void introduce(char *functionname);
-void test_test();
-void show_results();
